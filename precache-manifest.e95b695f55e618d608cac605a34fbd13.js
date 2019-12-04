b96232d75a960fd99f69dbeb6e1d01636af946ea
Codeaday <AJORIGAMI1@gmail.com>
2019-12-04 17:28:59 -0600

    Updates

---
+++ precache-manifest.e95b695f55e618d608cac605a34fbd13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0a65e2153bc11fde0c67f5d220c826",
+    "url": "/american-history-timeline/index.html"
+  },
+  {
+    "revision": "287f4a3d04b18a152907",
+    "url": "/american-history-timeline/static/css/2.221a2b37.chunk.css"
+  },
+  {
+    "revision": "d50390b19fd2b73e05a0",
+    "url": "/american-history-timeline/static/css/main.3cac18c4.chunk.css"
+  },
+  {
+    "revision": "287f4a3d04b18a152907",
+    "url": "/american-history-timeline/static/js/2.5d1f342c.chunk.js"
+  },
+  {
+    "revision": "d50390b19fd2b73e05a0",
+    "url": "/american-history-timeline/static/js/main.c07445d3.chunk.js"
+  },
+  {
+    "revision": "04b4d9eb26e6f02e5826",
+    "url": "/american-history-timeline/static/js/runtime-main.867a3bab.js"
+  }
+]);
